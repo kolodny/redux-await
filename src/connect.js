1bbf8635dc128d0caae9bea52f9c63ab473b9abb
Moshe Kolodny <kolodny.github@gmail.com>
2015-12-13 20:56:18 -0500

    try to ignore connect for coverage

--- src/connect.js
+++ src/connect.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 
+/* istanbul ignore next */
 export default (mapStateToProps, ...args) => {
   return connect(state => {
     const props = mapStateToProps(state);
