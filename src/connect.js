4d1674e03805b76c0e2bd92154907814fcee9a4b
Moshe Kolodny <kolodny.github@gmail.com>
2016-02-17 10:18:27 -0500

    pass in oldProps to react-redux's connect

--- src/connect.js
+++ src/connect.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 // can't really test this higher order function without overwriting the require cache
 /* istanbul ignore next */
 export default (mapStateToProps, ...args) => {
-  return connect(state => {
-    const props = mapStateToProps(state);
+  return connect((state, ownProps) => {
+    const props = mapStateToProps(state, ownProps);
     const { statuses, errors } = state.await;
     return { ...props, statuses, errors };
   }, ...args);
